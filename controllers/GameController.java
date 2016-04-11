218d75a4140d3b14b3b26f9b5e55843bb7807ffe
cyyim6723 <Charles@Sukis-MacBook-Air.local>
2016-04-11 21:18:17 +1000

    again and again

--- controllers/GameController.java
+++ controllers/GameController.java
@@ -7,19 +7,14 @@
  */
 package controllers;
 
-import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Iterator;
-import java.util.Map;
-
 import javax.swing.*;
 import models.*;
 import models.Explorer.*;
 import models.Guardians.*;
 import models.Item.*;
 import views.BoardView.Cell;
-import views.HudView;
 
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -27,86 +22,117 @@ import javax.swing.JPanel;
 import main.*;
 import views.*;
 
-public class GameController {
-    
-    //constants
-    private static final int ROWS = 12;
-    private static final int COLUMNS = 12;
-    private static enum State {DICE_ROLL, ACTION, CHECK_WIN} 
-    private State gameState;
-    
-    //models
-    private Game game;
-    //private Board board;
-    //private Player currentPlayer;
-    
-    //views
-    private MainMenuView menu;
-    private BoardView boardView;
-    private JFrame mainWindow;
-    private HudView hudView;
-    
-    //controllers
-    private final PlayerController playerController;
-    private final UnitController unitController;
-    private BoardController boardController;
-    
-    //Checks
-    private static boolean initExplorer;
-    private static boolean initGuardian;
-    private static boolean initBoard;
-    
-    
-    
-    private Player currentPlayer;
-    
-    public GameController(JFrame mainWindow){
-        this.mainWindow = mainWindow;
-        
-        playerController = new PlayerController(this);
-        //boardController = new BoardController(this);
-        unitController = new UnitController(this);
-        dice = new DiceUtility();
-        gameState = State.DICE_ROLL;
-        
-        //game = new Game(playerController.NewPlayer("Explorer"),playerController.NewPlayer("Guardian") );
-    }
-    
-    public void startGame(){
-        System.out.println("Start Game");
-        initGame();
-        
-        try{
-            System.out.println("Explorer: " + game.getPlayer("Explorer").getName());
-            System.out.println("Guardian: " + game.getPlayer("Guardian").getName());
-        }catch(Exception e){
-            System.out.println(e.getMessage());
-        }
-        
-        boardView = new BoardView(new BoardActionListener(),ROWS, COLUMNS);
-        mainWindow.getContentPane().add(boardView);
-        boardView.setVisible(true);
-        
-        hudView = new HudView();
-        mainWindow.getContentPane().add(hudView.hud, BorderLayout.SOUTH);
-        hudView.actionButton.addActionListener(new HUDActionListener());
-        
-        
-        
+public class GameController
+{
 
-    }
-    
-    
-    public void initGame(){
-    	Player guardian = getPlayerController().newPlayer("Guardian");
-        Player explorer = getPlayerController().newPlayer("Explorer");
-        initExplorerUnit(explorer);
-        initGuardianUnit(guardian);
-        
-        setCurrentPlayer(explorer);
-    }
-    
-    public void showMainMenu(){
+	// constants
+	private static final int ROWS = 12;
+	private static final int COLUMNS = 12;
+
+	private static enum State
+	{
+		DICE_ROLL, ACTION, CHECK_WIN
+	}
+
+	private State gameState;
+	// models
+	private Game game;
+	// private Board board;
+	// private Player currentPlayer;
+
+	// views
+	private MainMenuView menu;
+	private BoardView boardView;
+	private JFrame mainWindow;
+
+	// controllers
+	private final PlayerController playerController;
+	private final UnitController unitController;
+	private BoardController boardController;
+
+	// Checks
+	private static boolean initExplorer;
+	private static boolean initGuardian;
+	private static boolean initBoard;
+
+	private Player currentPlayer;
+
+	public GameController(JFrame mainWindow)
+	{
+		this.mainWindow = mainWindow;
+
+		playerController = new PlayerController(this);
+		// boardController = new BoardController(this);
+		unitController = new UnitController(this);
+		dice = new DiceUtility();
+		gameState = State.DICE_ROLL;
+
+		// game = new
+		// Game(playerController.NewPlayer("Explorer"),playerController.NewPlayer("Guardian")
+		// );
+	}
+
+	public void startGame()
+	{
+		System.out.println("Start Game");
+		BoardActionListener boardListener = new BoardActionListener();
+		initGame(boardListener);
+
+		try
+		{
+			System.out.println("Explorer: " + game.getPlayer("Explorer").getName());
+			System.out.println("Guardian: " + game.getPlayer("Guardian").getName());
+		} catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+
+		mainWindow.getContentPane().add(boardView);
+		mainWindow.pack();
+		boardView.setVisible(true);
+		turn(this.getCurrentPlayer());
+
+	}
+
+	public void initGame(BoardActionListener boardListener)
+	{
+		Player guardian = getPlayerController().newPlayer("Guardian");
+		Player explorer = getPlayerController().newPlayer("Explorer");
+		initBoardUnit(boardListener);
+		initExplorerUnit(explorer, boardListener);
+		initGuardianUnit(guardian, boardListener);
+		setCurrentPlayer(explorer);
+		
+
+		// TODO create players and units
+		// TODO create board object
+	}
+
+	public void showMainMenu()
+	{
+
+		MenuActionListener listener = new MenuActionListener();
+		menu = new MainMenuView(listener);
+		mainWindow.getContentPane().add(menu);
+
+		menu.setVisible(true);
+
+	}
+
+	public void turn(Player player)
+	{
+		// hudView.updateHUD(0, "Please roll dice", false);
+		player.setCurrentRoll(rollDice());
+		// hudView.updateHUD(player.getCurrentRoll(), "Please select unit",
+		// false);
+		// TODO select unit --> waiting --> if yes moveable (view), else no
+		// nothing
+		// hudView.updateHUD(player.getCurrentRoll(), "Where would you like to
+		// move?", true);
+		// TODO select destination --> waiting --> if yes move, else do nothing
+		// TODO ASSIGNMENT 2 - attack phase + ability phase
+		// TODO end
+	}
 
 	public void initExplorerUnit(Player player, BoardActionListener boardListener)
 	{
@@ -251,37 +277,13 @@ public class GameController {
 	{
 		System.out.println(unit.getX() +"       "+unit.getY());
 
-        }
-    
+	}
 
 	private void quitGame()
 	{
 		// System.exit(0);
 		return;
 	}
-    
-    private boolean checkWin(){
-    	//TODO Needs to be checked after updating to Taison's new code, at the moment it fails searching for a player
-    	Map unitMap = null;
-		try {
-			unitMap = game.getPlayer("explorer").units;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-    	
-		Iterator it = unitMap.entrySet().iterator();
-		   while (it.hasNext()) {
-		        Map.Entry pair = (Map.Entry)it.next();
-		        Unit unit = (Unit) pair.getValue();
-		        if (unit.pos[0] == 0 && unit.pos[1] == 0 || unit.pos[0] == 0 && unit.pos[1] == 1 || unit.pos[0] == 1 && unit.pos[1] == 0){
-		        	return(true);
-		        }
-		        it.remove(); // avoids a ConcurrentModificationException
-		    }
-    	
-    	
-    	return(false);
-    }
 
 	class MenuActionListener implements ActionListener
 	{
@@ -320,49 +322,5 @@ public class GameController {
 		}
 
 	}
-    
-    //Determines what actions should be completed when HUD button is pressed and instigates them
-    class HUDActionListener implements ActionListener {
-    	public void actionPerformed(ActionEvent e){
-    		//Move to action state
-    		if (gameState == State.DICE_ROLL){
-    			hudView.diceAmount.setText(String.valueOf(rollDice()));
-    			gameState = State.ACTION;
-    			hudView.setUnitState();
-    		} 
-    		//Move to check win state, restart if nobody won
-    		else if (gameState == State.ACTION){
-    			//Check if the player has won
-    			gameState = State.CHECK_WIN;
-    			boolean didWin = checkWin();
-    			if (!didWin){
-    				hudView.setDiceState();
-    				//Swap to the next player, this could be changed later to facilitate more than 2 players
-    				hudView.swapPlayer();
-    				if (getCurrentPlayer().getTeam() == "Explorer"){
-    					try {
-							setCurrentPlayer(game.getPlayer("guardian"));
-						} catch (Exception noPlayer) {
-							System.out.println("Guardian player not found");
-							noPlayer.printStackTrace();
-						}    					
-    				}
-    				
-    				else {
-    					try {
-							setCurrentPlayer(game.getPlayer("explorer"));
-						} catch (Exception noPlayer) {
-							System.out.println("Explorer player not found");
-							noPlayer.printStackTrace();
-						}    	
-    				}
-    				gameState = State.DICE_ROLL;
-    			}
-    			else {
-    				hudView.setWinState();
-    			}
-    		}
-    	}
-    }
 
 }
