821d54e693c4a53c1c4700017bac24019c3e2598
Jacob <parisjacob777@gmail.com>
2016-04-11 18:30:16 +1000

    Somehow reverted Boardview on git remote, fixing

--- views/BoardView.java
+++ views/BoardView.java
@@ -16,98 +16,79 @@ import models.Guardians.Hunter;
 import models.Item.*;
 
 
-public class BoardView extends JApplet {
+public class BoardView extends JPanel {
 	
-	int row = 12;
-	int column = 12;
-
 	// Create and initialize cells
-	private Cell[][] cells = new Cell[row][column];
-
-	// Create and initialize a status label
-	private JLabel jlblStatus = new JLabel("Start Game");
+	private Cell[][] cells;
 
 	/** Initialize UI */
-	public BoardView() {
-		// Panel p to hold cells
-		JPanel p = new JPanel(new GridLayout(row, column, 0, 0));
-		for (int y = 0; y < column; y++){
-			for (int x = 0; x < row; x++){
-				
-				if ((x==0 && y==0) || (x==0 && y==column-1) || 
-						(x==column-1 && y==0)) { // set Guardian start point
-					if (x==0 && y==0){
-						p.add(cells[y][x] = new Cell(y,x, new Behemoth(x,y)));
-					}
-					else if (x==0 && y==column-1){
-						p.add(cells[y][x] = new Cell(y,x, new Hunter(x,y)));
-					}
-					else if (x==column-1 && y==0){
-						p.add(cells[y][x] = new Cell(y,x, new Golem(x,y)));
-					}
-				}
-				
-			    else if ((x==0 && y==0) || (x==0 && y==1) ||
-						(x==1 && y==0)) { // set the gate color
-					p.add(cells[y][x] = new Cell(y,x, new Gate(x,y)));
-				}
-				
-				else if ((x==row-1 && y==column-1) || 
-						(x==row-1 && y==column-2) || 
-						(x==row-2 && y==column-1)|| 
-						(x==row-2 && y==column-2)) { // set Explorer start point
-					if (x==row-1 && y==column-1) {
-						p.add(cells[y][x] = new Cell(y,x, new Hero(x,y)));
-					}
-					else if	(x==row-1 && y==column-2) {
-						p.add(cells[y][x] = new Cell(y,x, new TrapMaster(x,y)));
-					}
-					else if (x==row-2 && y==column-1){
-						p.add(cells[y][x] = new Cell(y,x, new Tactician(x,y)));
-					}
-					else if (x==row-2 && y==column-2){
-						p.add(cells[y][x] = new Cell(y,x, new Scout(x,y)));
-					}
-				}
-					
-				else{
-					p.add(cells[y][x] = new Cell(y,x, new Ground(x,y)));
-				}
-			}
-		}
+	public BoardView(ActionListener listener, int rows, int columns) {
 		
-		// Set line borders on the cells panel and the status label
-		p.setBorder(new LineBorder(Color.black, 1));
-		jlblStatus.setBorder(new LineBorder(Color.yellow, 1));
+		super(new GridLayout(rows, columns, 0, 0));
+                
+                cells = new Cell[rows][columns];
 
-		// Place the panel and the label to the applet
-		add(p, BorderLayout.CENTER);
-		
-		//Add the 'HUD' bar at the bottom of the screen to provide instructions and buttons
-		add(new HudView().hud, BorderLayout.SOUTH);
-		
-		//Enable following line to show debug 'status' bar which shows information on cells clicked
-		//add(jlblStatus, BorderLayout.SOUTH);
-		
+                
+                initCells(listener, rows, columns);
+
+		// Set line borders on the cells panel and the status label
+		this.setBorder(new LineBorder(Color.black, 1));
 	}
 	
 	public Cell[][] getCells(){
 		return cells;
 	}
+
+    private void initCells(ActionListener listener, int rows, int columns) {
+        
+        for (int y = 0; y < columns; y++){
+            for (int x = 0; x < rows; x++) {
+
+                if ((x == 0 && y == 0) || (x == 0 && y == columns - 1)
+                        || (x == columns - 1 && y == 0)) { // set Guardian start point
+                    if (x == 0 && y == 0) {
+                        this.add(cells[y][x] = new Cell(y, x, new Behemoth(x, y), listener));
+                    } else if (x == 0 && y == columns - 1) {
+                        this.add(cells[y][x] = new Cell(y, x, new Hunter(x, y), listener));
+                    } else if (x == columns - 1 && y == 0) {
+                        this.add(cells[y][x] = new Cell(y, x, new Golem(x, y), listener));
+                    }
+                } else if ((x == 0 && y == 0) || (x == 0 && y == 1)
+                        || (x == 1 && y == 0)) { // set the gate color
+                    this.add(cells[y][x] = new Cell(y, x, new Gate(x, y), listener));
+                } else if ((x == rows - 1 && y == columns - 1)
+                        || (x == rows - 1 && y == columns - 2)
+                        || (x == rows - 2 && y == columns - 1)
+                        || (x == rows - 2 && y == columns - 2)) { // set Explorer start point
+                    if (x == rows - 1 && y == columns - 1) {
+                        this.add(cells[y][x] = new Cell(y, x, new Hero(x, y), listener));
+                    } else if (x == rows - 1 && y == columns - 2) {
+                        this.add(cells[y][x] = new Cell(y, x, new TrapMaster(x, y), listener));
+                    } else if (x == rows - 2 && y == columns - 1) {
+                        this.add(cells[y][x] = new Cell(y, x, new Tactician(x, y), listener));
+                    } else if (x == rows - 2 && y == columns - 2) {
+                        this.add(cells[y][x] = new Cell(y, x, new Scout(x, y), listener));
+                    }
+                } else {
+                    this.add(cells[y][x] = new Cell(y, x, new Ground(x, y), listener));
+                }
+            }
+        }
+    }
 	
 
 	// An inner class for a cell
-	public class Cell extends JPanel {
+	public class Cell extends JButton {
 		int x;
 		int y;
 		Unit unit;
 
-		public Cell(int x, int y, Unit unit) {
+		public Cell(int x, int y, Unit unit, ActionListener listener) {
 			this.x = x;
 			this.y = y;
 			this.unit = unit;
 			setBorder(new LineBorder(Color.black, 1)); // Set cell's border
-			addMouseListener(new MyMouseListener()); // Register listener
+			addActionListener(listener); // Register listener
 		}
 
 		/** Set a new token */
@@ -122,13 +103,10 @@ public class BoardView extends JApplet {
 			ImageIcon icon = unit.icon;
 			g.drawImage(icon.getImage(),0,0,getSize().width,getSize().height,this);
 		}
+                
+                public Unit getUnit(){
+                    return this.unit;
+                }
 
-		private class MyMouseListener extends MouseAdapter {
-			/** Handle mouse click on a cell */
-			public void mouseClicked(MouseEvent e) {
-				jlblStatus.setText(unit.getClass().getName() + "    Position: " + 
-			String.valueOf(x) + ", " + String.valueOf(y));
-			}
-		}
 	}
 }
