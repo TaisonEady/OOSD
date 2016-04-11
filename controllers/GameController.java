bc136a2e894c264abf117a5f94d9c9a9b1b9cca6
cyyim6723 <Charles@Sukis-MacBook-Air.local>
2016-04-11 19:54:06 +1000

    move initcell to gamecontroller

--- controllers/GameController.java
+++ controllers/GameController.java
@@ -22,13 +22,23 @@ import javax.swing.JPanel;
 import main.*;
 import views.*;
 
+<<<<<<< Updated upstream
 public class GameController {
+=======
+public class GameController
+{
+>>>>>>> Stashed changes
 
 	// constants
 	private static final int ROWS = 12;
 	private static final int COLUMNS = 12;
 
+<<<<<<< Updated upstream
 	private static enum State {
+=======
+	private static enum State
+	{
+>>>>>>> Stashed changes
 		DICE_ROLL, ACTION, CHECK_WIN
 	}
 
@@ -55,7 +65,12 @@ public class GameController {
 
 	private Player currentPlayer;
 
+<<<<<<< Updated upstream
 	public GameController(JFrame mainWindow) {
+=======
+	public GameController(JFrame mainWindow)
+	{
+>>>>>>> Stashed changes
 		this.mainWindow = mainWindow;
 
 		playerController = new PlayerController(this);
@@ -69,6 +84,7 @@ public class GameController {
 		// );
 	}
 
+<<<<<<< Updated upstream
 	public void startGame() {
 		System.out.println("Start Game");
 		initGame();
@@ -83,23 +99,57 @@ public class GameController {
 		boardView = new BoardView(new BoardActionListener(), ROWS, COLUMNS);
 		mainWindow.getContentPane().add(boardView);
 		mainWindow.pack();
+=======
+	public void startGame()
+	{
+		System.out.println("Start Game");
+		BoardActionListener boardListener = new BoardActionListener();
+		initGame(boardListener);
+
+		try
+		{
+			System.out.println("Explorer: " + game.getPlayer("Explorer").getName());
+			System.out.println("Guardian: " + game.getPlayer("Guardian").getName());
+		} catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+
+		mainWindow.getContentPane().add(boardView);
+>>>>>>> Stashed changes
 		boardView.setVisible(true);
 
 	}
 
+<<<<<<< Updated upstream
 	public void initGame() {
 		Player guardian = getPlayerController().newPlayer("Guardian");
 		Player explorer = getPlayerController().newPlayer("Explorer");
 		initExplorerUnit(explorer);
 		initGuardianUnit(guardian);
 
+=======
+	public void initGame(BoardActionListener boardListener)
+	{
+		Player guardian = getPlayerController().newPlayer("Guardian");
+		Player explorer = getPlayerController().newPlayer("Explorer");
+		initBoardUnit(boardListener);
+		initExplorerUnit(explorer, boardListener);
+		initGuardianUnit(guardian, boardListener);
+		
+>>>>>>> Stashed changes
 		setCurrentPlayer(explorer);
 
 		// TODO create players and units
 		// TODO create board object
 	}
 
+<<<<<<< Updated upstream
 	public void showMainMenu() {
+=======
+	public void showMainMenu()
+	{
+>>>>>>> Stashed changes
 
 		MenuActionListener listener = new MenuActionListener();
 		menu = new MainMenuView(listener);
@@ -109,7 +159,12 @@ public class GameController {
 
 	}
 
+<<<<<<< Updated upstream
 	public void turn(Player player) {
+=======
+	public void turn(Player player)
+	{
+>>>>>>> Stashed changes
 		// hudView.updateHUD(0, "Please roll dice", false);
 		player.setCurrentRoll(rollDice());
 		// hudView.updateHUD(player.getCurrentRoll(), "Please select unit",
@@ -123,6 +178,7 @@ public class GameController {
 		// TODO end
 	}
 
+<<<<<<< Updated upstream
 	public void initExplorerUnit(Player player) {
 		if (initExplorer) {
 			JOptionPane.showMessageDialog(null, "Explorer already initialize");
@@ -133,10 +189,30 @@ public class GameController {
 			Tactician tactician = new Tactician(0, 0);
 			TrapMaster trapMaster = new TrapMaster(0, 0);
 			try {
+=======
+	public void initExplorerUnit(Player player,BoardActionListener boardListener)
+	{
+		if (initExplorer)
+		{
+			JOptionPane.showMessageDialog(null, "Explorer already initialize");
+		} else
+		{
+			// initExplorerUnit
+			Hero hero = new Hero(11, 11);
+			Scout scout = new Scout(10, 10);
+			Tactician tactician = new Tactician(10, 11);
+			TrapMaster trapMaster = new TrapMaster(11, 10);
+			try
+			{
+>>>>>>> Stashed changes
 				player.addUnit("hero", hero);
+				boardView.initCells(boardListener, hero);
 				player.addUnit("scout", scout);
+				boardView.initCells(boardListener, scout);
 				player.addUnit("tactician", tactician);
+				boardView.initCells(boardListener, tactician);
 				player.addUnit("trapMaster", trapMaster);
+				boardView.initCells(boardListener, trapMaster);
 				initExplorer = true;
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
@@ -146,6 +222,7 @@ public class GameController {
 		}
 	}
 
+<<<<<<< Updated upstream
 	public void initGuardianUnit(Player player) {
 		if (initGuardian) {
 			JOptionPane.showMessageDialog(null, "Guardian already initialize");
@@ -156,9 +233,28 @@ public class GameController {
 			Hunter hunter = new Hunter(0, 0);
 
 			try {
+=======
+	public void initGuardianUnit(Player player,BoardActionListener boardListener)
+	{
+		if (initGuardian)
+		{
+			JOptionPane.showMessageDialog(null, "Guardian already initialize");
+		} else
+		{
+			// initGuardianUnit
+			Behemoth behemoth = new Behemoth(0, 0);
+			Golem golem = new Golem(11, 0);
+			Hunter hunter = new Hunter(0, 11);
+
+			try
+			{
+>>>>>>> Stashed changes
 				player.addUnit("behemoth", behemoth);
+				boardView.initCells(boardListener, behemoth);
 				player.addUnit("golem", golem);
+				boardView.initCells(boardListener, golem);
 				player.addUnit("hunter", hunter);
+				boardView.initCells(boardListener, hunter);
 				initGuardian = true;
 
 			} catch (Exception e) {
@@ -167,6 +263,7 @@ public class GameController {
 			}
 		}
 	}
+<<<<<<< Updated upstream
 
 	public void initBoardUnit() {
 		if (initBoard) {
@@ -225,6 +322,111 @@ public class GameController {
 			} else if (option == "options") {
 				System.out.println("show the options menu here.");
 			} else if (option == "quit") {
+=======
+
+	public void initBoardUnit(BoardActionListener boardListener)
+	{
+		if (initBoard)
+		{
+			JOptionPane.showMessageDialog(null, "Board already initialize");
+		} else
+		{
+			// initBoard
+			
+			boardView = new BoardView(boardListener, ROWS, COLUMNS);
+			for (int y = 0; y < COLUMNS; y++)
+			{
+				for (int x = 0; x < ROWS; x++)
+				{
+					BoardItem cell;
+					if ((x == 0 && y == COLUMNS - 1) || (x == COLUMNS - 1 && y == 0))
+					{ // set Guardian start point
+						cell = new GuardianStartPoint(x, y);
+					} else if ((x == ROWS - 1 && y == COLUMNS - 1) || (x == ROWS - 1 && y == COLUMNS - 2)
+					        || (x == ROWS - 2 && y == COLUMNS - 1) || (x == ROWS - 2 && y == COLUMNS - 2))
+					{ // set Explorer start point
+
+						cell = new ExplorerStartPoint(x, y);
+					} else if ((x == 0 && y == 0) || (x == 0 && y == 1) || (x == 1 && y == 0))
+					{ // set the gate color
+						cell = new Gate(x, y);
+					}
+
+					else
+					{
+						cell = new Ground(x, y);
+					}
+
+					boardView.initCells(boardListener, cell);
+				}
+			}
+
+			try
+			{
+
+				initBoard = true;
+
+			} catch (Exception e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public Player getCurrentPlayer()
+	{
+		return currentPlayer;
+	}
+
+	public void setCurrentPlayer(Player currentPlayer)
+	{
+		this.currentPlayer = currentPlayer;
+	}
+
+	private static DiceUtility dice;
+
+	// getter for testing only
+	public PlayerController getPlayerController()
+	{
+		return playerController;
+	}
+
+	public int rollDice()
+	{
+		return dice.roll();
+	}
+
+	private void cellClicked(Unit unit)
+	{
+		System.out.println(unit.getX()+"      "+unit.getY());
+
+	}
+
+	private void quitGame()
+	{
+		// System.exit(0);
+		return;
+	}
+
+	class MenuActionListener implements ActionListener
+	{
+
+		public void actionPerformed(ActionEvent e)
+		{
+
+			String option = ((JButton) e.getSource()).getName();
+
+			if (option == "startGame")
+			{
+				startGame();
+			} else if (option == "options")
+			{
+				System.out.println("show the options menu here.");
+			} else if (option == "quit")
+			{
+>>>>>>> Stashed changes
 				quitGame();
 			}
 
@@ -233,9 +435,17 @@ public class GameController {
 
 	}
 
+<<<<<<< Updated upstream
 	class BoardActionListener implements ActionListener {
 
 		public void actionPerformed(ActionEvent e) {
+=======
+	class BoardActionListener implements ActionListener
+	{
+
+		public void actionPerformed(ActionEvent e)
+		{
+>>>>>>> Stashed changes
 
 			Unit unit = ((Cell) e.getSource()).getUnit();
 
--- views/BoardView.java
+++ views/BoardView.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package views;
 
 import java.awt.*;
@@ -133,4 +134,128 @@ public class BoardView extends JPanel {
 			this.unit = unit;
 		}
 	}
+=======
+package views;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+import models.BoardItem;
+import models.Unit;
+import models.Explorer.Hero;
+import models.Explorer.Scout;
+import models.Explorer.Tactician;
+import models.Explorer.TrapMaster;
+import models.Guardians.Behemoth;
+import models.Guardians.Golem;
+import models.Guardians.Hunter;
+import models.Item.*;
+
+
+public class BoardView extends JPanel {
+	
+	// Create and initialize cells
+	private Cell[][] cells;
+
+	/** Initialize UI */
+	public BoardView(ActionListener listener, int rows, int columns) 
+	{
+		
+		super(new GridLayout(rows, columns, 0, 0));
+                
+                cells = new Cell[rows][columns];
+
+                
+//                initCells(listener, rows, columns);
+
+		// Set line borders on the cells panel and the status label
+		this.setBorder(new LineBorder(Color.black, 1));
+	}
+	
+	public Cell[][] getCells(){
+		return cells;
+	}
+
+    public void initCells(ActionListener listener, Unit cell) {
+    	if(cells[cell.getY()][cell.getX()] == null)
+    	this.add(cells[cell.getY()][cell.getX()] = new Cell(cell.getY(), cell.getX(), cell, listener));
+    	else
+    	{
+    		cells[cell.getY()][cell.getX()].setUnit(cell);
+    	}
+//        for (int y = 0; y < columns; y++){
+//            for (int x = 0; x < rows; x++) {
+//
+//                if ((x == 0 && y == 0) || (x == 0 && y == columns - 1)
+//                        || (x == columns - 1 && y == 0)) { // set Guardian start point
+//                    if (x == 0 && y == 0) {
+//                        this.add(cells[y][x] = new Cell(y, x, new Behemoth(x, y), listener));
+//                    } else if (x == 0 && y == columns - 1) {
+//                        this.add(cells[y][x] = new Cell(y, x, new Hunter(x, y), listener));
+//                    } else if (x == columns - 1 && y == 0) {
+//                        this.add(cells[y][x] = new Cell(y, x, new Golem(x, y), listener));
+//                    }
+//                } else if ((x == 0 && y == 0) || (x == 0 && y == 1)
+//                        || (x == 1 && y == 0)) { // set the gate color
+//                    this.add(cells[y][x] = new Cell(y, x, new Gate(x, y), listener));
+//                } else if ((x == rows - 1 && y == columns - 1)
+//                        || (x == rows - 1 && y == columns - 2)
+//                        || (x == rows - 2 && y == columns - 1)
+//                        || (x == rows - 2 && y == columns - 2)) { // set Explorer start point
+//                    if (x == rows - 1 && y == columns - 1) {
+//                        this.add(cells[y][x] = new Cell(y, x, new Hero(x, y), listener));
+//                    } else if (x == rows - 1 && y == columns - 2) {
+//                        this.add(cells[y][x] = new Cell(y, x, new TrapMaster(x, y), listener));
+//                    } else if (x == rows - 2 && y == columns - 1) {
+//                        this.add(cells[y][x] = new Cell(y, x, new Tactician(x, y), listener));
+//                    } else if (x == rows - 2 && y == columns - 2) {
+//                        this.add(cells[y][x] = new Cell(y, x, new Scout(x, y), listener));
+//                    }
+//                } else {
+//                    this.add(cells[y][x] = new Cell(y, x, new Ground(x, y), listener));
+//                }
+//            }
+//        }
+    }
+	
+
+	// An inner class for a cell
+	public class Cell extends JButton {
+		int x;
+		int y;
+		Unit unit;
+
+		public Cell(int x, int y, Unit unit, ActionListener listener) {
+			this.x = x;
+			this.y = y;
+			this.unit = unit;
+			setBorder(new LineBorder(Color.black, 1)); // Set cell's border
+			addActionListener(listener); // Register listener
+		}
+
+		/** Set a new token */
+		public void darwPos() {
+			repaint();
+		}
+
+		/** Paint the cell */
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			this.setBackground(Color.white); // background color
+			ImageIcon icon = unit.icon;
+			g.drawImage(icon.getImage(),0,0,getSize().width,getSize().height,this);
+		}
+                
+                public Unit getUnit(){
+                    return this.unit;
+                }
+                public void setUnit(Unit u)
+                {
+                	this.unit = u;
+                }
+
+	}
+>>>>>>> Stashed changes
 }
